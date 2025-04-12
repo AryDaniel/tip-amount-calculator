53b86e35320ae56ddc2c9667b95d6a578503d140
AryDaniel <ary.lopez@cetis107.edu.mx>
2025-04-12 14:30:21 -0700

    feat(order): integrate OrderContent component and implement order display with currency formatting

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -5,9 +5,21 @@ export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
     const addItem = (item: MenuItem) => {
-        console.log(item)
+        const itemExists = order.find(orderItem => orderItem.id === item.id)
+        if(itemExists) {
+            const updatedOrder = order.map(orderItem => orderItem.id === item.id ? 
+                {...orderItem, quantity: orderItem.quantity+1} : 
+                orderItem
+            )
+            setOrder(updatedOrder)
+        } else {
+           const newItem : OrderItem = {...item, quantity: 1}
+            setOrder([...order, newItem])
+        }
     }
+    
     return {
-        addItem 
+        order,
+        addItem
     }
 }
